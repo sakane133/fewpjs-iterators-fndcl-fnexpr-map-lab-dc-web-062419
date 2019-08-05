d73a4b9193e79771c0df1df66a062ac25f757a81
Samantha Kane <s.kane133@gmail.com>
2019-08-05 19:13:35 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function title(string){
+  let x = string.split(" ").map(function(word){
+    return word[0].toUpperCase() + word.slice(1);
+  })
+  return x.join(" ")
 }
+
+    function titleCased(){
+       return tutorials.map(function(tutorial){
+         return title(tutorial);
+         
+       })
+    }
